9b76d6e455e3358bd53280b395f5b198760e80b1
Gabriel Goulart <gabrielgoulartrms@gmail.com>
2022-10-26 13:22:46 -0300

    About required query params

--- main.py
+++ main.py
@@ -1,23 +1,45 @@
 from typing import Union
-
 from fastapi import FastAPI
 from pydantic import BaseModel
+from enum import Enum
 
 app = FastAPI()
 
-class Item(BaseModel):
-    name:str
-    price:float
-    is_offer:Union[bool, None] = None
+class ModelName(str, Enum):
+    alexnet = "alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
 
 @app.get("/")
-def read_root():
-    return {"Hello": "World"}
+async def read_root():
+    return {"message": "Hello World"}
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/models/{model_name}")
+async def get_model(model_name:ModelName):
+    if model_name is ModelName.alexnet:
+        return {"model_name": model_name, "message":"Deep learning FTW"}
+    if model_name.value == "lenet":
+        return {"model_name": model_name, "message":"LecNN all the images"}
+    return {"model_name": model_name, "message":"None of the above"}
 
-@app.put("/items/{item_id}")
-def update_item(item_id:int, item:Item):
-    return {"item_name": item.name, "item_id": item_id, "item_price":item.price, 'is_offer':item.is_offer}
+@app.get("/files/{file_path:path}")
+async def read_file(file_path: str):
+    return {"file_path": file_path}
+
+@app.get("/users/{user_id}/items/{item_id}")
+async def read_user_item(
+    user_id: int, item_id: str, needy: str, q: str | None = None, short: bool = False, skip: int = 0
+):
+    item = {"item_id": item_id, "owner_id": user_id, "skip":skip, "needy":needy}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update(
+            {"description": "This is an amazing item that has a long description"}
+        )
+    return item
+
+@app.get("/items/query_required/{item_id}")
+async def read_user_item(item_id: str, needy: str):
+    item = {"item_id": item_id, "needy": needy}
+    return item
